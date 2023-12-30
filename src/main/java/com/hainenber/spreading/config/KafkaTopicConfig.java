40375968358c3e72247f87e6ec2928ecedeb63de
hainenber <dotronghai96@gmail.com>
2023-12-30 21:51:51 +0700

    feat(*): setup cfg for Kafka integration

---
+++ src/main/java/com/hainenber/spreading/config/KafkaTopicConfig.java
@@ -0,0 +1,33 @@
+package com.hainenber.spreading.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value(value = "${hackernews.topic}")
+    private String hackernewsTopic;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        return new KafkaAdmin(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress));
+    }
+
+    @Bean
+    public NewTopic hackernews() {
+        return TopicBuilder.name(hackernewsTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
